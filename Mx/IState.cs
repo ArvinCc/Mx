583482b1bd8e1ca731af0db6b1e3d109a75b9d34
ArvinChenG <714332039@qq.com>
2017-08-29 09:55:53 +0800

    1.0.0.8
    未知

---
+++ Mx/IState.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mx
+{
+
+    namespace FSM 
+    {
+
+        interface IState
+        {
+            /// <summary>
+            /// 获取状态机的ID
+            /// </summary>
+            /// <returns></returns>
+            uint GetStateID();
+
+
+            /// <summary>
+            /// 进入这个状态
+            /// </summary>
+            /// <param name="machine">状态机</param>
+            /// <param name="prevState">上一个状态</param>
+            /// <param name="param1">参数1</param>
+            /// <param name="param2">参数2</param>
+            void OnEnter(StateMachine machine,IState prevState,object param1,object param2);
+
+
+            /// <summary>
+            /// 离开当前这状态
+            /// </summary>
+            /// <param name="nextState">下一个要进入的状态</param>
+            /// <param name="param1">参数1</param>
+            /// <param name="param2">参数2</param>
+            void OnLeave(IState nextState,object param1,object param2);
+
+
+            void OnUpdate();
+
+
+            void OnFixedUpdate();
+
+
+            void OnLeteUpdate();
+
+        }
+    }
+}
---
+++ Mx/StateMachine.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mx
+{
+
+    namespace FSM
+    {
+        class StateMachine
+        {
+
+            public StateMachine()
+            {
+                mStateDic = new Dictionary<uint, IState>();
+            }
+
+            /// <summary>
+            /// 注册
+            /// </summary>
+            /// <param name="state">状态</param>
+            /// <returns>是否成功</returns>
+            public bool RegisterState(IState state)
+            {
+                if (state == null)
+                {
+                    // Debug.LogError("StateMachine.RegisterState state=null");
+                    return false;
+                }
+
+                if (mStateDic.ContainsKey(state.GetStateID()))
+                {
+                    //Debug.LogError("StateMachine.RegisterState mStateDic have this key,  key=" + state.GetStateID());
+                    return false;
+                }
+
+                mStateDic.Add(state.GetStateID(), state);
+                return true;
+            }
+
+            /// <summary>
+            /// 注销状态
+            /// </summary>
+            /// <param name="stateId">状态id</param>
+            /// <returns>是否成功</returns>
+            public bool RemoveState(uint stateId)
+            {
+                if (!mStateDic.ContainsKey(stateId))
+                {
+                    return false;
+                }
+                if (mCurrentState != null && mCurrentState.GetStateID() == stateId)
+                {
+                    return false;
+                }
+                
+                mStateDic.Remove(stateId);
+                
+                return true;
+            }
+
+            public bool SwitchState(uint newStateId,object param1,object param2)
+            {
+
+                if(mCurrentState !=null && mCurrentState.GetStateID()==newStateId)
+                {
+                    return false;
+                }
+
+                IState newState = null;
+                mStateDic.TryGetValue(newStateId, out newState);
+
+                if (newState == null)
+                {
+                    return false;
+                }
+
+                if (mCurrentState != null)
+                {
+                    mCurrentState.OnLeave(newState, param1, param2);
+                }
+
+
+                return true;
+            }
+
+
+
+            public IState CurrentState
+            {
+                get
+                {
+                    return mCurrentState;
+                }
+            }
+
+            public uint CurrentID
+            {
+                get
+                {
+                    return mCurrentState == null ? 0 : mCurrentState.GetStateID();
+                }
+            }
+
+            public IState OldState
+            {
+                get
+                {
+                    return mOldState;
+                }
+            }
+
+            public uint OldID
+            {
+                get
+                {
+                    return mOldState == null ? 0 : mOldState.GetStateID();
+                }
+            }
+
+            public void OnUpdate()
+            {
+                if (mCurrentState == null)
+                    return;
+
+                mCurrentState.OnUpdate();
+            }
+
+            public void OnFixedUpdate()
+            {
+                if (mCurrentState == null)
+                    return;
+
+                mCurrentState.OnFixedUpdate();
+            }
+
+
+            public void OnLeteUpdate()
+            {
+                if (mCurrentState == null)
+                    return;
+
+                mCurrentState.OnLeteUpdate();
+            }
+
+
+            private Dictionary<uint, IState> mStateDic = null;
+
+            private IState mCurrentState = null;
+
+            private IState mOldState = null;
+        }
+    }
+}
